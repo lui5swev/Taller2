de7612339d90b8670423ea1319286b89d28d78c7
lui5swev <l.abarzua01@ufromail.cl>
2023-03-30 15:30:17 -0300

    trabajo avance

--- src/main/java/taller2.java
+++ src/main/java/taller2.java
@@ -1,5 +1,72 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class taller2 {
     public static void main(String[] args) {
+        double[][] notas = new double[50][5];
+        menu(notas);
+    }
 
+    private static void menu(double[][] notas){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("MENU");
+        System.out.println("Que accion desea realizar? Ingrese el numero correspondiente");
+        System.out.println("[1] Agregar Estudiante");
+        System.out.println("[2] Mostrar Estudiantes Aprobados");
+        System.out.println("[3] Mostrar Estudiantes Reprobados");
+        System.out.println("[4] Mostrar Estudiantes que van a examen");
+        System.out.println("[5] Mostrar estado de Estudiantes");
+        System.out.println("[6] Salir");
+        String respuesta = teclado.next();
+
+        /*switch (respuesta){
+            case "1": agregarEstudiante(notas);
+                break;
+            case "2": mayorSismo(notas);
+                break;
+            case "3": contarSismos5(notas);
+                break;
+            case "4": enviarSMS(notas);
+                break;
+            case "5": irni3nv(notas);
+                break;
+            case "6": System.exit(0);
+                break;
+            default:
+                System.out.println("Ingrese una opcion valida");
+                menu(notas);
+                break;
+        }
+        menu(notas);*/
+    }
+
+    private static void agregarEstudiante(double[][] notas) {
+        Random random = new Random();
+        for (int i = 0; i < 50; i++) {
+            if(notas[i][0]==0)
+                for (int j = 0; j < 5; j++) {
+                    notas[i][j] = random.nextDouble(1.0, 7.0);
+                }
+            break;
+            }
+        }
+
+    private static void estudiantesAprobados(double[][] notas){
+        double[] ponderaciones = {0.25, 0.25, 0.25, 0.15, 0.10};
+
+        for (int i = 0; i < 50; i++) {
+            double suma = 0;
+            if (notas[i][0]!=0)
+                for (int j = 0; j < 5; j++) {
+                    suma += notas[i][j]*ponderaciones[j];
+                }
+                if (suma>=4.0) {
+                    System.out.println("Estudiante #" + (i + 1) + " Aprobo");
+                }
+                suma = 0;
+            }
+        }
     }
 }
+
+
